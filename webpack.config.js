e51b912ca5a1ecb9886b7b9c27f793f6d5e4ee1e
Amarpreet Singh <amarpreet2209@gmail.com>
2017-09-28 12:32:29 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: 'src/index.html', to: 'dist/index.html' }
+        ])
+    ]
+};
+
+module.exports = config;
