bf5d54fc334f826152360e1b8c13fbd250a8abeb
Amarpreet Singh <amarpreet2209@gmail.com>
2017-09-28 12:41:40 +0530

    5

--- src/app/index.js
+++ src/app/index.js
@@ -30,51 +30,77 @@
 
 // render(<App />, window.document.getElementById('app'));
 
-import {createStore} from 'redux';
+import { createStore, combineReducers } from "redux";
 
-const initialState = {
-    result: 1,
-    lastValues: []
-}
+const mathReducer = (state = {
+  result: 1,
+  lastValues: []
+}, action) => {
+  switch (action.type) {
+    case "ADD":
+      state = {
+        ...state,
+        result: state.result + action.payload,
+        lastValues: [...state.lastValues, action.payload]
+      };
 
-const reducer = (state = initialState, action) => {
-    switch(action.type) {
-        case "ADD": 
-            state = {
-                ...state,
-                result: state.result + action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            };
-            
-            break;
-        case "SUBTRACT":
-            state = {
-                    ...state,
-                    result: state.result - action.payload,
-                    lastValues: [...state.lastValues, action.payload]
-            };        
-            break;
-    }
-    return state;
+      break;
+    case "SUBTRACT":
+      state = {
+        ...state,
+        result: state.result - action.payload,
+        lastValues: [...state.lastValues, action.payload]
+      };
+      break;
+  }
+  return state;
 };
 
-const store = createStore(reducer);
+const userReducer = (state = {
+  name: 'Max',
+  age: 27
+}, action) => {
+  switch (action.type) {
+    case "SET_NAME":
+      state = {
+        ...state,
+        name: action.payload
+      };
+
+      break;
+    case "SET_AGE":
+      state = {
+        ...state,
+        age: action.payload
+      };
+      break;
+  }
+  return state;
+};
+
+const store = createStore(combineReducers({mathReducer, userReducer}));
 
 store.subscribe(() => {
-    console.log("Store updated!", store.getState());
+  console.log("Store updated!", store.getState());
 });
 
 store.dispatch({
-    type: "ADD",
-    payload: 100
+  type: "ADD",
+  payload: 100
 });
 
 store.dispatch({
-    type: "ADD",
-    payload: 22
+  type: "ADD",
+  payload: 22
 });
 
 store.dispatch({
-    type: "SUBTRACT",
-    payload: 80
+  type: "SUBTRACT",
+  payload: 80
+});
+
+
+store.dispatch({
+  type: "SET_AGE",
+  payload: 30
 });
