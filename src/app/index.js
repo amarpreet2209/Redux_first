a4ddb77a361a516d04f4049d556580ca795b528a
Amarpreet Singh <amarpreet2209@gmail.com>
2017-09-28 12:55:38 +0530

    6

--- src/app/index.js
+++ src/app/index.js
@@ -30,7 +30,9 @@
 
 // render(<App />, window.document.getElementById('app'));
 
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware
+} from "redux";
+import logger from 'redux-logger';
 
 const mathReducer = (state = {
   result: 1,
@@ -78,10 +80,18 @@ const userReducer = (state = {
   return state;
 };
 
-const store = createStore(combineReducers({mathReducer, userReducer}));
+const myLogger = (store) => (next) => (action) => {
+    console.log("Logged Action: ", action);
+    next(action);
+};
+
+const store = createStore(
+    combineReducers({mathReducer, userReducer}), 
+    {}, 
+    applyMiddleware(logger));
 
 store.subscribe(() => {
-  console.log("Store updated!", store.getState());
+//   console.log("Store updated!", store.getState());
 });
 
 store.dispatch({
